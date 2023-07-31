7154418e4a331dda592c167e2cc43677fa92dd40
adibrisan <adi.brisan@yahoo.com>
2023-07-31 12:39:34 +0300

    Created basic cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,23 @@
-import "./App.css";
-import { Box } from "@chakra-ui/react";
+import styles from "./App.module.css";
+import RoomCard from "./components/RoomCard/RoomCard";
 
 function App() {
   return (
-    <Box
-      bg="#001e39"
-      h="100vh"
-      w="100%"
-      pos="relative"
-      _before={{
-        content: '""',
-        display: "block",
-        position: "absolute",
-        top: "0",
-        left: "0",
-        right: "0",
-        bottom: "0",
-        backgroundImage: `
-        linear-gradient(to right, #404040 1px, transparent 1px)
-        `,
-        backgroundPosition: "0 0, 0 0",
-        backgroundSize: "40px 40px",
-        zIndex: 1,
-      }}
-      _after={{
-        content: '""',
-        display: "block",
-        position: "absolute",
-        top: "0",
-        left: "0",
-        right: "0",
-        bottom: "0",
-        backgroundImage: `
-          linear-gradient(#404040 1px, transparent 1px),
-          linear-gradient(90deg, #CBD5E0 1px, transparent 1px)
-        `,
-        backgroundPosition: "0 0, 0 0",
-        backgroundSize: "40px 40px",
-        zIndex: 1,
-      }}
-    />
+    <div className={styles.center}>
+      <div className={styles.cardContainer}>
+        <RoomCard
+          title="Create a room"
+          inputText="Enter your name"
+          buttonTitle="Let's create the room"
+        />
+        <RoomCard
+          title="or join a room"
+          inputText="Enter the room id"
+          buttonTitle="Join this room"
+          mode="join"
+        />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/RoomCard/RoomCard.tsx
@@ -0,0 +1,88 @@
+import { useState, ChangeEvent, useEffect } from "react";
+import {
+  Box,
+  Heading,
+  Input,
+  Button,
+  VStack,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+interface IRoomCard {
+  title: string;
+  buttonTitle: string;
+  inputText: string;
+  mode?: "create" | "join";
+}
+
+const RoomCard = ({
+  title,
+  buttonTitle,
+  inputText,
+  mode = "create",
+}: IRoomCard) => {
+  const marginValue = useBreakpointValue({ base: "20px", md: "0", lg: "20px" });
+  const [userName, setUserName] = useState("");
+  const [showError, setShowError] = useState(false);
+
+  const handleUserNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+
+  useEffect(() => {
+    if (userName.trim() !== "") {
+      setShowError(false);
+    }
+  }, [userName]);
+
+  const handleCreateRoom = () => {
+    // Implement your logic to create the room with the user name
+    if (userName.trim() === "") {
+      setShowError(true);
+    } else {
+      console.log("Creating room with user name:", userName);
+    }
+  };
+
+  return (
+    <Box
+      width="300px"
+      height="200px"
+      borderWidth="1px"
+      borderRadius="lg"
+      p={5}
+      margin={marginValue}
+      boxShadow="lg"
+    >
+      <Heading as="h2" size="lg" mb={4} color="whiteAlpha.800" pb={10}>
+        {title}
+      </Heading>
+
+      <VStack spacing={20} position="relative">
+        <Input
+          placeholder={inputText}
+          value={userName}
+          onChange={handleUserNameChange}
+          focusBorderColor="teal.500"
+          color="#ffffff"
+          size="lg"
+          sx={{
+            borderColor: showError ? "red.500" : "inherit",
+            "::placeholder": { color: "white" },
+          }}
+        />
+        {showError && (
+          <Box color="red.500" fontSize="xl" top={35} position="absolute">
+            {mode === "create" ? "Name is required" : "Room id is required"}
+          </Box>
+        )}
+
+        <Button colorScheme="teal" onClick={handleCreateRoom}>
+          {buttonTitle}
+        </Button>
+      </VStack>
+    </Box>
+  );
+};
+
+export default RoomCard;
